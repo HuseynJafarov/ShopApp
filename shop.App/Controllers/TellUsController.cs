6abc229de26f5e28bdc7e9c360bc65d9ed660e52
HuseynJafarov <huseynnj@code.edu.az>
2023-05-25 16:56:11 +0400

    AllControllers

--- shop.App/Controllers/TellUsController.cs
+++ shop.App/Controllers/TellUsController.cs
@@ -1,6 +1,60 @@
-﻿namespace shop.App.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Service.Interface;
+using System.ComponentModel.DataAnnotations;
+
+namespace shop.App.Controllers
 {
-    public class TellUsController
+    public class TellUsController:AppController
     {
+        private readonly ITellusService _tellusService;
+
+        public TellUsController(ITellusService tellusService)
+        {
+            _tellusService = tellusService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            await _tellusService.GetAllAsync();
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SoftDelete([Required] int id)
+        {
+            try
+            {
+                await _tellusService.SoftDeleteAsync(id);
+                return Ok();
+            }
+            catch (NullReferenceException)
+            {
+                return NotFound();
+            }
+
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([Required] int id)
+        {
+            try
+            {
+                await _tellusService.DeleteAsync(id);
+                return Ok();
+            }
+            catch (NullReferenceException)
+            {
+                return NotFound();
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Search(string? searchData)
+        {
+            await _tellusService.SerachAsync(searchData);
+            return Ok();
+        }
     }
 }
