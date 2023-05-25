10be76b3e8a5bdf42504e6f8a95b4ecfe6ed90f8
HuseynJafarov <huseynnj@code.edu.az>
2023-05-25 16:54:37 +0400

    TellUs

--- shop.App/Controllers/SubscribeController.cs
+++ shop.App/Controllers/SubscribeController.cs
@@ -1,6 +1,61 @@
-﻿namespace shop.App.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.DTOs.SliderBox;
+using Service.Service.Interface;
+using System.ComponentModel.DataAnnotations;
+
+namespace shop.App.Controllers
 {
-    public class SubscribeController
+    public class SubscribeController:AppController
     {
+        private readonly ISubscribeService _subscribeService;
+
+        public SubscribeController(ISubscribeService subscribeService)
+        {
+            _subscribeService = subscribeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            await _subscribeService.GetAllAsync();
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SoftDelete([Required] int id)
+        {
+            try
+            {
+                await _subscribeService.SoftDeleteAsync(id);
+                return Ok();
+            }
+            catch (NullReferenceException)
+            {
+                return NotFound();
+            }
+
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([Required] int id)
+        {
+            try
+            {
+                await _subscribeService.DeleteAsync(id);
+                return Ok();
+            }
+            catch (NullReferenceException)
+            {
+                return NotFound();
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Search(string? searchData)
+        {
+            await _subscribeService.SerachAsync(searchData);
+            return Ok();
+        }
     }
 }
