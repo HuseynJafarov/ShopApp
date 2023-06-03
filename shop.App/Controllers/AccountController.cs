2c646f392f0115c2de5036a29226c3d0b6603c62
HuseynJafarov <huseynnj@code.edu.az>
2023-06-03 20:00:40 +0400

    AccountController

--- shop.App/Controllers/AccountController.cs
+++ shop.App/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Service.DTOs.Account;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Service.Service.Interface;
+using Services.DTOs.Account;
+using Services.Helpers.Responses;
 
 namespace shop.App.Controllers
 {
@@ -14,8 +16,10 @@ namespace shop.App.Controllers
         }
 
         [HttpPost]
-        public async Task<ApiResponse> Register([FromBody] RegisterDto register)
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        public async Task<RegisterResponse> Register([FromBody] RegisterDto register)
         {
+           
             return await _accountService.RegisterAsync(register);
         }
 
@@ -27,6 +31,47 @@ namespace shop.App.Controllers
 
 
         [HttpPost]
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        public async Task<IActionResult> CreateRole()
+        {
+            await _accountService.CreateRoleAsync();
+            return Ok();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        public async Task<IActionResult> AddRoleToUser([FromBody] UserRoleDto model)
+        {
+            await _accountService.AddRoleToUserAsync(model);
+            return Ok();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetRolesByUser(string userId)
+        {
+            return Ok(await _accountService.GetRolesByUserAsync(userId));
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetRoles()
+        {
+            return Ok(await _accountService.GetRolesAsync());
+        }
+
+        [HttpGet]
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(statusCode: StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> GetUsers()
+        {
+            return Ok(await _accountService.GetUsersAsync());
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
         public async Task<IActionResult> Login([FromBody] LoginDto login)
         {
             var result = await _accountService.LoginAsync(login);
