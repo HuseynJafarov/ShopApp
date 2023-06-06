5befe0e4350d4517cc4c59d326e8ce8308098f7b
HuseynJafarov <huseynnj@code.edu.az>
2023-06-06 17:44:43 +0400

    Author Controller

--- shop.App/Controllers/AuthorController.cs
+++ shop.App/Controllers/AuthorController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Service.DTOs.Author;
 using Service.Service.Interface;
+using System.ComponentModel.DataAnnotations;
 
 namespace shop.App.Controllers
 {
@@ -16,6 +18,68 @@ namespace shop.App.Controllers
         public async Task<IActionResult> GetAllWithCarts()
         {
             return Ok(await _service.GetAllAsyncWithCarts());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AuthorCreateAndUpdateDto author)
+        {
+            await _service.CreateAsync(author);
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _service.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SoftDelete([Required] int id)
+        {
+            try
+            {
+                await _service.SoftDeleteAsync(id);
+                return Ok();
+            }
+            catch (NullReferenceException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([Required] int id)
+        {
+            try
+            {
+                await _service.DeleteAsync(id);
+                return Ok();
+            }
+            catch (NullReferenceException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([Required][FromRoute] int id,  AuthorCreateAndUpdateDto author)
+        {
+            try
+            {
+                await _service.UpdateAsync(id, author);
+                return Ok();
+            }
+            catch (NullReferenceException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Search(string? search)
+        {
+            return Ok(await _service.SerachAsync(search));
         }
     }
 }
